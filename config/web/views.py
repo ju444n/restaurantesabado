bb668e009a8dc596be7e9464e441379fc29e6060
209 <209@academicos.cesde.edu.co>
2022-10-29 13:03:09 -0500

    ok

--- config/web/views.py
+++ config/web/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 
+from web.formularios.formularioPlatos import FormularioPlatos
+
 # Create your views here.
 
 #TODAS LAS VISTAS SON FUNCIONES DE PYTHON
 
 def Home(request):
     return render(request,'home.html')
+
+def Platos(request):
+
+    #Esta vista va a utilizar un formulario de django
+    #DEBO CREAR ENTONCES UN OBJETO DE LA CLASE FormularioPlatos()
+    formulario=FormularioPlatos()
+
+    #CREAMOS UN DICCIONARIO PARA ENVIAR EL FORMULARIO AL HTML(TEMPLATE)
+    data={
+        'formulario':formulario
+    }
+
+    return render(request,'menuplatos.html',data)
